65fe672f2d8d62e0554b633fd004b65bf26230ba
Rishav <rishavgh97@gmail.com>
2020-07-24 20:39:04 -0700

    Add file reading capability
    pid_junction_node is able to read PID parameters from a text file
    placed in the resources folder. A sample text file has been added
    to the folder.

--- jasper_controller/src/pid_junction_node.cpp
+++ jasper_controller/src/pid_junction_node.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #include "jasper_controller/pid_junction_node.h"
 #include "jasper_msgs/DynamicsInput.h"
@@ -6,35 +8,61 @@
 #include "message_filters/sync_policies/approximate_time.h"
 #include "message_filters/synchronizer.h"
 #include "ros/ros.h"
+#include "ros/package.h"
 
-PID_Junction::PID_Junction(const ros::Publisher& pub_input, const ros::Publisher& pub_second)
-  : pub(pub_input), err_pub(pub_second)
+PID_Junction::PID_Junction(const ros::Publisher &pub_input, const ros::Publisher &pub_second)
+    : pub(pub_input), err_pub(pub_second)
 {
   Kp = Eigen::DiagonalMatrix<double, 6>();
   Kd = Eigen::DiagonalMatrix<double, 6>();
 
-  Kp.diagonal() << 100, 200, 500, 1200, 3300, 140000;
-  Kd.diagonal() << 50, 50, 50, 70, 90, 80;
+  // Reading PID params from file
+  std::string file_path = ros::package::getPath("jasper_controller");
+  file_path += "/resources/pid_params.txt";
+
+  std::ifstream param_file(file_path);
+  if (param_file.is_open())
+  {
+    //Kp
+    std::string temp_str;
+
+    for (int i = 0; i < 6; ++i)
+    {
+      param_file >> temp_str;
+      Kp(i, i) = std::stod(temp_str);
+    }
+
+    //Kd
+    for (int i = 0; i < 6; ++i)
+    {
+      param_file >> temp_str;
+      Kd(i, i) = std::stod(temp_str);
+    }
+  }
+  else
+  {
+    ROS_ERROR("Could not find pid_params.txt file! Please make sure that the file is placed in the resources folder of the jasper_controller package. Setting default PID params.");
+    Kp.diagonal() << 100, 200, 500, 1200, 3300, 140000;
+    Kd.diagonal() << 50, 50, 50, 70, 90, 80;
+  }
 }
 
 template <typename Derived>
-void PID_Junction::DetermineCompensation(const Eigen::MatrixBase<Derived>& joint_err,
-                                         const Eigen::MatrixBase<Derived>& vel_err,
-                                         const Eigen::MatrixBase<Derived>& acc)
+void PID_Junction::DetermineCompensation(const Eigen::MatrixBase<Derived> &joint_err,
+                                         const Eigen::MatrixBase<Derived> &vel_err,
+                                         const Eigen::MatrixBase<Derived> &acc)
 {
   jointPosCompensation = Kp * joint_err;
   jointVelCompensation = Kd * vel_err;
   jointAccCompensation = acc;
 }
 
-void PID_Junction::pidCallback(const jasper_msgs::JointInfo::ConstPtr& command_msg,
-                               const jasper_msgs::JointInfo::ConstPtr& feedback_msg)
+void PID_Junction::pidCallback(const jasper_msgs::JointInfo::ConstPtr &command_msg,
+                               const jasper_msgs::JointInfo::ConstPtr &feedback_msg)
 {
   Eigen::Matrix<double, 6, 1> joint_err;
   Eigen::Matrix<double, 6, 1> vel_err;
   Eigen::Matrix<double, 6, 1> acc;
-
-  std::cout << "PID Callback" << std::endl;
 
   joint_err = stdVectorToMatrix(command_msg->jointAngles) - stdVectorToMatrix(feedback_msg->jointAngles);
   vel_err = stdVectorToMatrix(command_msg->jointVelocities) - stdVectorToMatrix(feedback_msg->jointVelocities);
@@ -57,7 +85,7 @@ void PID_Junction::pidCallback(const jasper_msgs::JointInfo::ConstPtr& command_m
   pub.publish(dyn_msg);
 }
 
-Eigen::Matrix<double, 6, 1> PID_Junction::stdVectorToMatrix(const std::vector<double>& vec)
+Eigen::Matrix<double, 6, 1> PID_Junction::stdVectorToMatrix(const std::vector<double> &vec)
 {
   const int cols = vec.size();
   Eigen::Matrix<double, 6, 1> result;
@@ -68,7 +96,7 @@ Eigen::Matrix<double, 6, 1> PID_Junction::stdVectorToMatrix(const std::vector<do
   return result;
 }
 
-std::vector<double> PID_Junction::matrixToStdVector(const Eigen::Matrix<double, 6, 1>& mat)
+std::vector<double> PID_Junction::matrixToStdVector(const Eigen::Matrix<double, 6, 1> &mat)
 {
   std::vector<double> result;
 
@@ -78,7 +106,7 @@ std::vector<double> PID_Junction::matrixToStdVector(const Eigen::Matrix<double, 
   return result;
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "pid_junction_node");
 
@@ -86,7 +114,7 @@ int main(int argc, char** argv)
 
   ros::Publisher pub = nh.advertise<jasper_msgs::DynamicsInput>("pid_command", 10);
   ros::Publisher error_pub =
-      nh.advertise<jasper_msgs::JointInfo>("joint_error", 10);  // To tune the PID by plotting the error
+      nh.advertise<jasper_msgs::JointInfo>("joint_error", 10); // To tune the PID by plotting the error
 
   PID_Junction pid(pub, error_pub);
 
